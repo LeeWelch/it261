1b622e9130f12b9be73323fe98a1b22e540ae2f5
LeeWelch <146404478+LeeWelch@users.noreply.github.com>
2024-02-26 02:51:55 -0800

    Adds week 8 files

---
+++ it261/weeks/week8/credentials.php
@@ -0,0 +1,7 @@
+<?php    // my credentials that will allow me to connect to the database!!!
+
+define('DB_NAME', 'wjdftate_it261');
+define('DB_USER', 'wjdftate_erw4627');
+define('DB_PASSWORD', 'Rilakkuma261');
+define('DB_HOST', 'mysql.leewelch.net');
+
