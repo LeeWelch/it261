13db473a0cacc90c85dc8ea3e6bdceb61864db92
LeeWelch <146404478+LeeWelch@users.noreply.github.com>
2024-02-26 23:01:55 -0800

    Updates portal page

--- it261/index.php
+++ it261/index.php
@@ -18,7 +18,7 @@
             <li><a href="website/calculator.php">Calculator</a></li>
             <li><a href="website/contact.php">Email</a></li>
             <li><a href="">Database</a></li>
-            <li><a href="">Gallery</a></li>
+            <li><a href="website/gallery.php">Gallery</a></li>
         </ul>
 </nav>
 
