e9ad77da52b60dc93b38cceed4e82105e9999bc6
LeeWelch <146404478+LeeWelch@users.noreply.github.com>
2024-03-11 17:03:36 -0700

    Add files via upload

--- it261/weeks/week9/includes/header.php
+++ it261/weeks/week9/includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Week Nine Homework</title>
+    <title>Login and Register Assignment</title>
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
